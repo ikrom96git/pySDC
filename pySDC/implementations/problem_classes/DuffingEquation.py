56126e45acef91742a15541584c31a403324d42f
Ikrom Akramov <ikrom.akramov@tuhh.de>
2024-12-04 16:14:27 +0100

    needs to write a test and documentation

--- pySDC/implementations/problem_classes/DuffingEquation.py
+++ pySDC/implementations/problem_classes/DuffingEquation.py
@@ -5,80 +5,118 @@ from pySDC.core.errors import ProblemError
 from pySDC.implementations.problem_classes.HarmonicOscillator import harmonic_oscillator
 from pySDC.implementations.datatype_classes.particles import particles, acceleration
 from scipy.integrate import solve_ivp
-class duffingequation(Problem):
-    dtype_u=particles
-    dtype_f=acceleration
 
-    def __init__(self, omega=1.0, b=1.0, epsilon=0.1, u0=(1,0)):
-        u0=np.asarray(u0)
+
+class duffingequation(Problem):
+
+    r"""
+    Example implementing the Duffing equation 
+
+    .. math::
+        \ddot{x}+\omega^2 x+\varepsilon b x^{3}=0
+    with initial conditions are 
+
+    .. math::
+        x(0)=a, \quad v(0)=\dot{x}(0)=0
+    which is a second-order problem. The unknown function :math:`x` denotes the position of the mass, and the
+    derivative is the velocity. :math:`\mu` defines the damping and :math:`k` is the spring constant.
+
+    Parameters
+    ----------
+    k : float, optional
+        Spring constant :math:`k`.
+    mu : float, optional
+        Damping parameter :math:`\mu`.
+    u0 : tuple, optional
+        Initial condition for the position, and the velocity. Should be a tuple, e.g. ``u0=(1, 0)``.
+    phase : float, optional
+        Phase of the oscillation.
+    amp : float, optional
+        Amplitude of the oscillation.
+    Source: https://beltoforion.de/en/harmonic_oscillator/
+    """
+
+    dtype_u = particles
+    dtype_f = acceleration
+
+    def __init__(self, omega=1.0, b=1.0, epsilon=0.1, u0=(2, 0)):
+        u0 = np.asarray(u0)
         super().__init__((1, None, np.dtype("float64")))
-        self._makeAttributeAndRegister('omega', 'b', 'u0', 'epsilon', localVars=locals(), readOnly= True)
+        self._makeAttributeAndRegister(
+            'omega', 'b', 'u0', 'epsilon', localVars=locals(), readOnly=True)
 
     def eval_f(self, u, t):
-        
-        me=self.dtype_f(self.init)
-        me[:]=-self.omega**2*u.pos-self.epsilon*self.b*u.pos**3
+
+        me = self.dtype_f(self.init)
+        me[:] = -self.omega**2*u.pos-self.epsilon*self.b*u.pos**3
 
         return me
-    
+
     def u_init(self):
 
-        u0=self.u0
-        u=self.dtype_u(self.init)
-        u.pos[0]=u0[0]
-        u.vel[0]=u[1]
+        u0 = self.u0
+        u = self.dtype_u(self.init)
+        u.pos[0] = u0[0]
+        u.vel[0] = u[1]
+        return u
 
     def u_exact(self, t):
         raise ProblemError('Exact solution of Duffing equation does not exist')
-    
-    def scipy_solve_ivp(self, t):
-        
+
+    def duffing_rhs(self, t, u):
+        return [u[1], -(self.omega**2)*u[0]-self.epsilon*self.omega*u[0]**3]
+
+    def scipy_solve_ivp(self, Tend, t_span):
+        u = self.dtype_u(self.init)
+        solve = solve_ivp(self.duffing_rhs, [0, Tend], self.u0, t_eval=t_span)
+        solution = solve.y
+        u.pos = solution[0, :]
+        u.vel = solution[1, :]
+        return u
+
 
 class duffing_zeros_model(harmonic_oscillator):
-    def __init__(self, omega=1.0, u0=(1, 0)):
+    def __init__(self, omega=1.0, u0=(2, 0)):
         super().__init__(k=omega**2, u0=u0)
-    
-   
+
+
 class duffing_first_model(duffingequation):
-    def __init__(self, omega=1, b=1, epsilon=0.1, u0=(1, 0)):
+    def __init__(self, omega=1, b=1, epsilon=0.1, u0=(0, 0)):
         super().__init__(omega, b, epsilon, u0)
+        self.duffing_zeros = duffing_zeros_model()
 
     def eval_f(self, u, t):
-        me=self.dtype_f(self.init)
-        z=t*self.omega
-        righthandside=-(0.25*self.u0[0]*self.b)*(np.cos(3*z)+3*np.cos(z))
+        me = self.dtype_f(self.init)
+        z = t*self.omega
+        righthandside = -(0.25*self.u0[0]*self.b)*(np.cos(3*z)+3*np.cos(z))
 
-        me[:]=-self.omega**2*u.pos+righthandside
+        me[:] = -self.omega**2*u.pos+righthandside
         return me
-    
+
     def u_init(self):
-        
-        u=self.dtype_u(self.init)
-        u.pos[0]=0.0
-        u.vel[0]=0.0
+
+        u = self.dtype_u(self.init)
+        u.pos[0] = 0.0
+        u.vel[0] = 0.0
         return u
-    
+
     def u_exact(self, t):
-        me=self.dtype_u(self.init)
 
-        z=self.omega*t
+        me_first = self.dtype_u(self.init)
+        M1 = -(self.duffing_zeros.u0[0]**3*self.b)/(32*self.omega**2)
+        N1 = 0.0
+        z = self.omega*t
+        me_first.pos = M1 * np.cos(z) + N1 * np.sin(z) + \
+            M1 * (12 * z * np.sin(z) - np.cos(3 * z))
+        me_first.vel = (-M1 * self.omega * np.sin(z) + N1 * self.omega * np.cos(z) +
+                        M1 * self.omega * (12 * np.sin(z) + 12 * z * np.cos(z) + 3 * np.sin(3 * z)))
 
-        me.pos=(self.u0[0]**3*self.b*np.sin(z))*(np.sin(2*z)+6*z)/(16*self.omega**2)
-        me.vel=-(self.u0[0]*self.b/(32*self.omega))*(3*np.sin(3*z)+11*np.sin(z)+12*z*np.cos(z))
+        return me_first
+
+    def u_asymptotic_solution(self, t):
+        me = self.dtype_u(self.init)
+        u_zeros = self.duffing_zeros.u_exact(t)
+        u_first = self.u_exact(t)
+        me.pos = u_zeros.pos+self.epsilon*u_first.pos
+        me.vel = u_zeros.vel+self.epsilon*u_first.vel
         return me
-
-    def u_asymptotic_expan(self, u_zeros, u_first):
-        me=self.dtype_u(self.init)
-        me.pos=u_zeros.pos+self.epsilon*u_first.pos
-        me.vel=u_zeros.vel+self.epsilon*u_first.vel
-        return me
-
-if __name__=='__main__':
-    problem_params=dict()
-    problem_params['k']=1.0
-    problem_params['mu']=0.0
-    problem_params['u0']=np.array([1, 0])
-    zeros=duffing_zeros_model()
-    u_ex=zeros.u_exact(0.1)
-    breakpoint()
-    print(u_ex)
--- pySDC/playgrounds/Boris/duffingequation_playground.py
+++ pySDC/playgrounds/Boris/duffingequation_playground.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pySDC.implementations.problem_classes.DuffingEquation import duffingequation, duffing_zeros_model, duffing_first_model
-
+import matplotlib.pyplot as plt
 def duffing_eqaution_default_params():
     problem_params=dict()
     problem_params['omega']=1.0
@@ -12,5 +12,52 @@ def duffing_eqaution_default_params():
 
 def solution_Duffing_equation():
     problem_params=duffing_eqaution_default_params()
-    duffing=duffingequation(problem_params)
-    
+    duffing=duffingequation()
+    t0=0.0
+    tend=2*np.pi 
+    time=np.linspace(t0, tend, 1000)
+    duffing_solution=duffing.scipy_solve_ivp(tend, time)
+    return duffing_solution.pos
+def solution_duffing_zeros_model():
+    duffing_zeros=duffing_zeros_model()
+    t0=0.0
+    tend=2*np.pi 
+    time=np.linspace(t0, tend, 1000)
+    u_pos=[]
+    for t in time:
+        solution=duffing_zeros.u_exact(t)
+        u_pos=np.append(u_pos, solution.pos)
+    return u_pos, time
+def solution_duffing_first_model():
+    duffing_first=duffing_first_model()
+    t0=0.0
+    tend=2*np.pi 
+    time=np.linspace(t0, tend, 1000)
+    u_pos=[]
+    for t in time:
+        solution=duffing_first.u_asymptotic_solution(t)
+        u_pos=np.append(u_pos, solution.pos)
+    return u_pos, time
+
+def plot_solution(Solution, time, label):
+    for nn in range(len(label)):
+        plt.plot(time, Solution[nn], label=label[nn])
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+if __name__=='__main__':
+    solution_ivp=solution_Duffing_equation()
+    zeros_model_sol, time=solution_duffing_zeros_model()
+    first_models_sol, time=solution_duffing_first_model()
+    Solution=[solution_ivp, zeros_model_sol, first_models_sol]
+    label=['solution_ivp', 'zeros model', 'first model']
+    plot_solution(Solution, time, label)
+
+
+
+
+
