fb003ea3ff4c876b73725b14968ba1bf882bc50e
Ikrom Akramov <ikrom.akramov@tuhh.de>
2024-12-05 15:30:29 +0100

    Added some documentation

--- pySDC/tests/test_problems/test_DuffingEquation.py
+++ pySDC/tests/test_problems/test_DuffingEquation.py
@@ -10,7 +10,12 @@ from qmat.lagrange import LagrangeApproximation
 
 @pytest.mark.base
 def test_DuffingEquation():
+    """
+    Testing eval_f for the Duffing equation
 
+    Returns:
+        _type_: _description_
+    """
     P = duffingequation()
 
     def derivative_eval_f(u):
@@ -28,11 +33,14 @@ def test_DuffingEquation():
 
     derivative_f = derivative_eval_f(nodes)
     approx_derivative = D1 @ func_vals
-    assert np.allclose(derivative_f, approx_derivative, atol=1e-6)
+    assert np.allclose(derivative_f, approx_derivative, atol=1e-6), 'eval_f function derivative is failed'
 
 
 @pytest.mark.base
 def test_reduced_models():
+    """
+    Test for first order reduced model RHS funciton
+    """
     P = duffing_first_model()
 
     def derivative_eval_f(t):
@@ -40,7 +48,7 @@ def test_reduced_models():
         return (0.25 * (P.duffing_zeros.u0[0] ** 3) * P.b * P.omega) * (3 * np.sin(3 * z) + 3 * np.sin(z))
 
     num_nodes = 5
-    nodes = np.sort(np.random.rand(num_nodes) / 16)
+    nodes = np.sort(np.random.rand(num_nodes) / 8)
     approx = LagrangeApproximation(nodes)
     D1 = approx.getDerivationMatrix()
     u = P.u_init()
@@ -50,11 +58,14 @@ def test_reduced_models():
 
     derivative_f = derivative_eval_f(nodes)
     approx_derivative = D1 @ func_vals
-    assert np.allclose(derivative_f, approx_derivative, atol=1e-6)
+    assert np.allclose(derivative_f, approx_derivative, atol=1e-6), 'Approximate derivative and derivative of RHS function do not reat tolerence!'
 
 
 @pytest.mark.base
 def test_exact_solution():
+    """
+    Test for the exact solution of the reduced order models
+    """
     P_duffing = duffingequation(epsilon=0.001)
     P_reduced_model = duffing_first_model(epsilon=0.001)
 
@@ -67,7 +78,7 @@ def test_exact_solution():
         u_asymp = P_reduced_model.u_asymptotic_expansion(ii)
         scipy_solution = np.append(scipy_solution, u_scipy.pos)
         asymp_solution = np.append(asymp_solution, u_asymp.pos)
-    assert np.allclose(scipy_solution, asymp_solution, atol=1e-6)
+    assert np.allclose(scipy_solution, asymp_solution, atol=1e-6), 'Scipy solution and asymptotic solution are failed!'
 
 
 if __name__ == '__main__':
